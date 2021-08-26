563ec80e8977d3a5ef65607977082a0febe333ce
Aviv-Yaari <avivyar@gmail.com>
2021-08-26 17:44:36 +0300

    email: styles and dummy data updates

--- js/services/dummydata.service.js
+++ js/services/dummydata.service.js
@@ -9,18 +9,41 @@ const DUMMY_EMAILS = [
     id: utilService.makeId(),
     status: 'inbox',
     subject: 'Hello My Friend',
-    body: 'Would love to catch up sometimes',
+    body: `Welcome to Google Cloud.
+Learn the fundamentals with this tutorial – and see what else you can do for free on Google Cloud with our Always Free tier.
+Go to your Console
+
+Welcome to your Google Cloud free trial. Beginning today, you have $300 USD in credit to spend on Google Cloud. With your free trial, you can:
+	
+Use your credits to evaluate the platform risk-free*
+	
+Explore a wide range of Google Cloud products and services – from Compute Engine and BigQuery to App Engine and industry-leading AI
+	
+Easily check your credit usage by visiting the Cloud Billing section of your Google Cloud Console
+See what’s always free.
+Before diving into your free trial, take a moment to explore over 20 always-free products in our Always Free tier. There’s no charge or credit cost to use these products up to their specified usage limit.
+Get hands-on now with this quick Cloud Console Tour, highlighting how to easily navigate, organize, and manage your projects. To start exploring on your own, just visit your Cloud Console.
+Go to your Console
+See you in the cloud,
+The Google Cloud Team
+* Your free trial stays free. There’s no charge unless you turn on automatic billing. If you use all of your free credits, or want to work on your projects beyond the term of your trial, we save your projects for 30 days.`,
     isRead: false,
     isStarred: false,
     sentAt: 1629962876683,
     to: 'momo@momo.com',
-    from: 'user@appsus.com',
+    from: 'CloudPlatform-noreply@google.com',
   },
   {
     id: utilService.makeId(),
     status: 'inbox',
-    subject: 'This is Muki',
-    body: 'Would love to catch up sometimes',
+    subject: 'It’s here. Arturia FX Collection 2',
+    body: `THAT NEW NEW NEW
+
+Arturia never disappoints. Like never. Their FX Collection 2 comes at us with it’s classic blend of vintage effects and modern enhancers, now with 3 new Bus FX, 4 new Modulation FX, an updated preset browser, and 200 new presets. FX Collection 2 puts exceptional studio-quality effects in the hands of musicians and producers of all styles and abilities.
+
+Try Free
+
+`,
     isRead: false,
     isStarred: true,
     sentAt: 1629490199000,
@@ -30,8 +53,20 @@ const DUMMY_EMAILS = [
   {
     id: utilService.makeId(),
     status: 'inbox',
-    subject: 'I love you !!!',
-    body: 'Would love to catch up sometimes',
+    subject: 'Action Required: Your Characters At Risk for Deletion',
+    body: `Dear Mapler,
+
+We haven’t seen you in awhile...
+
+We’re writing to let you know that at least one character on your account has been flagged and is at risk of being deleted for inactivity. This clean-up is scheduled for June 9, 2021
+
+What Can you Do?
+
+To keep your characters, simply log into your account at least once before the June clean-up and your characters will be safe!
+
+We did want to make sure we emailed you to give you a heads up. For more information, please check our Lapsed Character Clean-Up post on the Official MapleStory website.
+
+The MapleStory Team`,
     isRead: true,
     isStarred: true,
     sentAt: 1629876579000,
@@ -41,7 +76,260 @@ const DUMMY_EMAILS = [
   {
     id: utilService.makeId(),
     status: 'inbox',
-    subject: 'Miss you (inbox)!',
+    subject: 'Wanna record great vocals from home?',
+    body: `With Chris Liepe’s course, Perfect Vocal Takes, reopening for students this Tuesday...
+
+It’s time we pulled the curtain back and let you have a look inside the course, to see what it’s all about.
+
+With that, we’re going to give you one of the 3 in-depth vocal walkthroughs inside the course…
+
+Where you’ll get to see Chris revisit an old song he’s made as a producer/vocalist for hire, and then re-track and craft a compelling, professional vocal performance, all while explaining what he’s doing at every step of the process.
+
+(You’ll find the link to the walkthrough at the bottom of this email)
+
+We like to think that the best vocal takes were made by a singer rolling up to the mic half asleep, and magic just comes out of their mouth…
+
+But unless you’re a gifted vocal talent - that’s simply not possible. 
+
+But by applying the critical, yet artistic approach that Chris shows inside this walkthrough, you’ll see how he puts together a cohesive, emotionally captivating performance as a result.
+
+So what’s inside Perfect Vocal Takes? 
+
+Inside Chris’s course, “Perfect Vocal Takes”, you’ll see him break down the crucial mindsets needed to craft amazing vocal takes, warm up your voice, explore its registers, and then be shown how Chris crafts a perfect vocal take in 3 separate tracks.
+
+The course consists of 3 in-depth modules that are structured to help you discover and develop your vocal recording skills, and achieve great vocal performances from a home studio. 
+
+Module 1 gives you The Winning Vocal Mindsets that Chris has discovered over years of vocal trial and error and coaching hundreds of students to discover their voice. 
+
+You’ll be shown how to create a “Perfect Vocal Chain” that gives you the flexibility and power of an expensive recording studio right from the comfort of your home studio…
+
+How to make sure your recording environment is set up to capture the best vocal recordings possible…
+
+As well as the #1 mindset that stops most vocalists from getting great vocal performances in the first place. 
+
+Module 2 is all about Warming Up & Exploring Your Voice, where Chris reveals his best vocal exercises to get you loose on the microphone, and have your voice warm and ready for recording a great performance. 
+
+You’ll discover how to warm up by “warming down”, as well as how to feel out your voice’s range and power on recording day itself… 
+
+A simple “countdown exercise” that gets you energized and ready to record vocals with…
+
+And Chris’s relied-on vocal effects to make recording feel as effortless as singing in the shower.
+
+This module will show you how to protect your vocal health when recording, and how to dial in your voice on recording day. 
+
+Finally, Module 3 is all about Real World Song Case Studies, where Chris shows you just how to craft great vocal takes in 3 songs of completely different genres. 
+
+From a tender acoustic singer-songwriter song to an upbeat rock track, and then a heavy metal track, you’ll be shown how to nail a professional, compelling vocal in a variety of genres.
+
+You’ll be shown: 
+
+How to create seamless vocal performances even if you have to punch in takes to create the best performance on tape
+Why most vocal comps don’t sound as good as they should be (and the simple fix to this)
+How to craft different types of voices and supporting vocal tracks in your arrangements to have a powerful, engaging vocal performance, even if you’re not confident in your vocal skills
+What storytelling has to do with giving vocal performances that stand out from your first word to your last
+Why vocal arranging is far simpler than it looks, and simple ways to turn a lead vocal into a powerful choir without burning your voice out
+And what makes the case studies so powerful is that Chris will literally record and create these vocal performances in front of you.
+
+You’ll see him fail, you’ll see him nail takes, you’ll see him listen back, analyze, and point out things that most vocalists wouldn’t spot when recording their own voice. 
+
+You’ll see how he builds vocal arrangements from the ground up, and have a complete insider’s look on how to craft amazing vocals from your home studio. 
+
+By the time you’re done with Perfect Vocal Takes - your vocals will never be the same ever again.
+
+So how much will Perfect Vocal Takes cost? 
+
+Well, a private lesson with Chris Liepe costs $300…
+
+And while Perfect Vocal Takes costs $147, as part of Chris Liepe collaborating with the Recording Revolution this month… 
+
+We’ll be reopening Perfect Vocal Takes at the massively discounted price of just $67 for lifetime access to the course. 
+
+We’ll be opening up the course this Tuesday, but for now…
+
+We’ve got one last sneak peek from Perfect Vocal Takes for you.
+
+It’s an entire vocal case study where Chris crafts a perfect vocal take for an upbeat rock song from start to finish. 
+
+You’ll see how he approaches the lead vocal, how he builds the backing vocal arrangements, and adds ad-libs and characters in the vocal performance to create something extraordinary.
+
+Click here to see Chris craft a perfect vocal take for an upbeat rock song. 
+
+Here’s to your music!
+
+P.S. We'll be sending you the link to check out Perfect Vocal Takes on Tuesday, so keep an eye out on your inbox for that!`,
+    isRead: true,
+    isStarred: false,
+    sentAt: 1551133930594,
+    to: 'momo@momo.com',
+    from: 'user@appsus.com',
+  },
+  {
+    id: utilService.makeId(),
+    status: 'inbox',
+    subject: 'Welcome to Positive Grid!',
+    body: `Hey ,
+
+Welcome to Positive Grid! From now on, you'll get regular updates on product news and special offers. In the meantime, here's a $10 coupon code for your first purchase! But hurry, your offer expires in seven days.
+ 
+Your Code:
+
+WELCOME10
+*coupon available for desktop products (PC/Mac) only.
+Questions? Contact us!
+
+
+Thank you,
+Positive Grid`,
+    isRead: true,
+    isStarred: false,
+    sentAt: 1551133930594,
+    to: 'momo@momo.com',
+    from: 'it+noreply@positivegrid.com',
+  },
+  {
+    id: utilService.makeId(),
+    status: 'inbox',
+    subject: 'Last chance to get even bigger savings than Black Friday',
+    body: `
+Bigger savings than Black Friday
+Don't miss out on our exclusive Early Bird Sale, with even lower prices than the upcoming Black Friday Sale.
+
+ 
+Shop now
+ 
+ 
+ 
+ADpak bundles
+Get more, save more. Expand your ADpak library with new drum kits and sonic options. Choose without stress - you can select your content at any time.
+
+ 
+ADpak Bundles - 5 ADpak Bundle: $154.98 ($30.99 per ADpak) - 3 ADpak Bundle: $109.98 ($36.66 per ADpak) - 2 ADpak Bundle: $79.98 ($39.99 per ADpak)
+ 
+ 
+ 
+MIDIpak bundles
+Fuel your inspiration with new drum beats and full songs performed by amazing drummers. The more you get, the more you save. Choose freely - you can select your content at any time.
+
+ 
+MIDIpak Bundles - 10 MIDIpak Bundle: $64.98 ($6.49 per MIDIpak) - 7 MIDIpak Bundle: $54.98 ($7.85 per MIDIpak) - 5 MIDIpak Bundle: $44.98 ($8.99 per MIDIpak)
+ 
+ 
+ 
+Get better savings on Addictive Drums 2 and Addictive Trigger products
+ADpaks: $44.98 - Save $44.98	
+MIDIpaks: $9.98 - Save $9.98	
+ 
+Kitpiece Paks: $4.98 - Save $4.98	
+Addictive Trigger: 50% off all Addictive Trigger products	
+ 
+ 
+Sale ends Sunday November 15`,
+    isRead: true,
+    isStarred: false,
+    sentAt: 1551133930594,
+    to: 'momo@momo.com',
+    from: 'user@appsus.com',
+  },
+  {
+    id: utilService.makeId(),
+    status: 'inbox',
+    subject: 'My 4 laws of acoustic guitar recording',
+    body: `When it comes to recording acoustic guitar even the best and brightest of us tend to get confused.
+
+We end up recording acoustic guitar tracks that sound either overly harsh or terribly boomy - or somehow both!
+
+I want to help you out.
+
+Today I'm sharing my 4 "laws" of acoustic guitar recording. Consider these tried and true starting points for getting your best sound possible. Feel free to follow all of them - or only one or two. Whatever helps.
+
+​WATCH: 4 Laws of Acoustic Guitar Recording​
+
+
+Thanks for being a subscriber!
+
+-Graham`,
+    isRead: true,
+    isStarred: false,
+    sentAt: 1551133930594,
+    to: 'momo@momo.com',
+    from: 'user@appsus.com',
+  },
+  {
+    id: utilService.makeId(),
+    status: 'inbox',
+    subject: 'WANT MORE FREE NIGHTS?',
+    body: `
+WANT MORE FREE NIGHTS?
+
+Happy International Friendship Day!
+
+We've got a special treat for you ;)
+
+More Friends
+=
+More FREE Nights
+
+
+Hey Aviv
+
+We want to get you partying with your friends!!! 
+
+Are you still deciding on whether to take the leap and ESCAPE TO PARADISE? Check out all the awesome perks for pass holders and the unreal discounted bed nights to make your decision a no brainer.
+
+
+ESCAPE TO PARADISE PASSES
+PLUS, in honor of FRIENDSHIP DAY, we’ve got some awesome news for you about the chance to get EVEN MORE nights free. 
+
+
+
+WANT EVEN MORE FREE NIGHTS (you said)?
+
+Well, why not get your favorite travel mates to escape with you! Our friends referral program is launching now and we want you to cash in. 
+
+Get 3 Friends = 1 FREE Extra Week
+
+Get 5 Friends = 2 FREE Extra Weeks 
+
+It’s simple. All they have to do is use the unique code you'll receive once you book YOUR pass, when booking their Escape to Paradise pass. Whatever pass you or they purchase, you’ll still get credit for the referral and when you hit the 3 or 5 friend mark...Voila, you’ll be issued your FREE week(s) to use in addition to your pass. 
+
+ 
+So (huhmm, Book your pass!), SHARE THE NEWS, bring your crew, and get accruing tons of free nights. You’re welcome!!
+
+Love, 
+
+The Collective Crew`,
+    isRead: true,
+    isStarred: false,
+    sentAt: 1551133930594,
+    to: 'momo@momo.com',
+    from: 'hello@slumberpartyhostels.com',
+  },
+  {
+    id: utilService.makeId(),
+    status: 'inbox',
+    subject: 'Miss you! (sent)',
+    body: 'Would love to catch up sometimes',
+    isRead: true,
+    isStarred: false,
+    sentAt: 1551133930594,
+    to: 'momo@momo.com',
+    from: 'user@appsus.com',
+  },
+  {
+    id: utilService.makeId(),
+    status: 'inbox',
+    subject: 'Miss you! (sent)',
+    body: 'Would love to catch up sometimes',
+    isRead: true,
+    isStarred: false,
+    sentAt: 1551133930594,
+    to: 'momo@momo.com',
+    from: 'user@appsus.com',
+  },
+  {
+    id: utilService.makeId(),
+    status: 'inbox',
+    subject: 'Miss you! (sent)',
     body: 'Would love to catch up sometimes',
     isRead: true,
     isStarred: false,
