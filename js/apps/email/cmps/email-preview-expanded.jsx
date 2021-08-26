37b03906a7d3219c9c5a9f2764791db8119f7bae
Aviv-Yaari <avivyar@gmail.com>
2021-08-26 11:51:57 +0300

    email: added fullscreen btn, improved display for preview 2

--- js/apps/email/cmps/email-preview-expanded.jsx
+++ js/apps/email/cmps/email-preview-expanded.jsx
@@ -1,11 +1,12 @@
 import { utilService } from '../../../services/util.service.js';
 
 export const EmailPreviewExpanded = (props) => {
-  const { sentAt, body, from } = props.email;
+  const { sentAt, body, from, to, status } = props.email;
   return (
     <section className="email-preview-expanded flex column">
       <div className="email-header">
-        <div>From: {from}</div>
+        {status === 'inbox' && <div>From: {from}</div>}
+        {(status === 'sent' || status === 'draft') && <div>To: {to}</div>}
         <div>Sent at: {utilService.formatDate(sentAt)}</div>
       </div>
       <div>{body}</div>
