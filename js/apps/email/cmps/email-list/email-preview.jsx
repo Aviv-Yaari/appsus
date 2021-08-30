49ec66d9fc11bb696a78cdd25a71f41904cf55f2
Aviv-Yaari <avivyar@gmail.com>
2021-08-30 11:17:01 +0300

    emails: fixed status field on preview all mails

--- js/apps/email/cmps/email-list/email-preview.jsx
+++ js/apps/email/cmps/email-list/email-preview.jsx
@@ -28,9 +28,9 @@ export class EmailPreview extends React.Component {
         <section className="email-mini flex align-center">
           <div className="email-star-subject flex align-center">
             <BtnStar onToggle={(ev) => onValueToggle(ev, email, 'isStarred')} email={email} />
-            {currStatus === 'all' && <span className="email-status">{email.status}</span>}
             <span className="email-subject">
-              {utilService.trimText(email.subject, 100) || 'no subject'}
+              {(currStatus === 'all' && `[${email.status}] `) +
+                utilService.trimText(email.subject, 100) || 'no subject'}
             </span>
           </div>
           <div className="email-date">{utilService.formatDate(email.sentAt)}</div>
